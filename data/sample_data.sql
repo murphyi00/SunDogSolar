5688613a4c19a9783e0e75303410275752854832
murphyi00 <71462707+murphyi00@users.noreply.github.com>
2024-09-13 16:17:50 -0700

    Update sample_data.sql

--- data/sample_data.sql
+++ data/sample_data.sql
@@ -1,39 +1,67 @@
 -- SunDogSolar: Sample Data
 
--- Sample data for regions
+-- Insert Regions (Sample)
 INSERT INTO regions (region_name)
 VALUES 
-('Northeast'), 
-('South'), 
-('Midwest'), 
-('West');
+('Northeast'),
+('South');
 
--- Sample data for power providers
+-- Insert Power Providers (Sample)
 INSERT INTO power_providers (provider_name, provider_resale_plan)
 VALUES
 ('Pacific Gas & Electric', 0.25),
-('Consolidated Edison', 0.29),
-('Duke Nukem Energy', 0.50),
-('Neon Cloud Light & Power', 0.12),
-('Greater Barad-dûr Regional Utilities', 0.45),
 ('Sunshine Rainbow Power', 0.36);
 
--- Sample data for solar panels
+-- Insert Solar Panels Lookup (Sample)
 INSERT INTO solar_panels_lookup (solpanel_price, solpanel_mfgr, solpanel_dims, solpanel_elec_spec)
 VALUES
 (450.32, 'Janus Component Mfg', '30x30', '25 kWh'),
-(150.12, 'Econofail Inc.', '29x29', '15 kWh'),
-(616.22, 'ACME Coyote Industries', '40x40', '125 kWh'),
-(420.00, 'Better Green Products', '40x20', '42 kWh'),
 (999.32, 'Biggs Ripov Supplies', '50x50', '16 kWh');
 
--- Sample data for state lookup
+-- Insert State Lookup (Sample)
 INSERT INTO state_lookup (state_id, state_name, state_region_id)
 VALUES
-('AK', 'Alaska', 4),
-('CA', 'California', 4),
-('TX', 'Texas', 2),
 ('NY', 'New York', 1),
-('FL', 'Florida', 2),
-('IL', 'Illinois', 3);
+('TX', 'Texas', 2);
 
+-- Insert Zipcodes (Sample)
+INSERT INTO zipcodes (zipcode_id, zipcode_state_id)
+VALUES 
+(18064, 'NY'),
+(77345, 'TX');
+
+-- Insert Leads (Sample)
+INSERT INTO leads (lead_provider_id, lead_electric_bill_amt, lead_roof_flat, lead_roof_sqrft, lead_zipcode, lead_status)
+VALUES 
+(1, 337.15, 1, 295, 18064, 'NEW'),
+(2, 312.55, 1, 200, 77345, 'NEW');
+
+-- Insert Salespersons (Sample)
+INSERT INTO salespersons (salesperson_email, salesperson_firstname, salesperson_lastname, salesperson_region_id)
+VALUES 
+('goodshepard@sundog.com', 'Eve', 'Adams', 1),
+('ghayes@sundog.com', 'Rock', 'Rivers', 2);
+
+-- Insert Customers (Sample)
+INSERT INTO customers (customer_firstname, customer_lastname, customer_address1, customer_email, customer_city, customer_state_id, customer_salesperson_id, customer_lead_id)
+VALUES 
+('Larry', 'Karlmann', '123 Merry Lane', 'lkarforless@superrito.com', 'Syracuse', 'NY', 1, 1),
+('Steve', 'Boate', '1110 Wicker Heights Blvd', 'sboate@drift.com', 'Houston', 'TX', 2, 2);
+
+-- Insert Proposals (Sample)
+INSERT INTO proposals (proposal_lead_id, proposal_salesperson_id, proposal_panel_type, proposal_panels_qty, proposal_total_cost)
+VALUES 
+(1, 1, 'Panasonic', 10, 15000),
+(2, 2, 'Siemens', 12, 18000);
+
+-- Insert Installers (Sample)
+INSERT INTO installers (installer_email, installer_state_id)
+VALUES 
+('otyme@dayrep.com', 'NY'),
+('meyezing@dayrep.com', 'TX');
+
+-- Insert Bids (Sample)
+INSERT INTO bids (bid_installer_id, bid_customer_id, bid_amount, bid_status, bid_date)
+VALUES 
+(1, 1, 2001.16, 'NEW', '2023-08-21'),
+(2, 2, 2306.12, 'INACTIVE', '2021-05-01');
