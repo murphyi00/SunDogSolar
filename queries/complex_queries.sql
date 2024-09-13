9105e7df4862c9d224a0e6c799be775c2f59613f
murphyi00 <71462707+murphyi00@users.noreply.github.com>
2024-09-13 16:13:21 -0700

    Update complex_queries.sql

--- queries/complex_queries.sql
+++ queries/complex_queries.sql
@@ -1,1 +1,22 @@
+-- SunDogSolar: Complex Queries
+
+-- Retrieve all leads along with their corresponding salespersons and regions
+WITH LeadDetails AS (
+    SELECT l.lead_id, l.lead_electric_bill_amt, l.lead_roof_sqrft, sp.salesperson_firstname, sp.salesperson_lastname, r.region_name
+    FROM leads l
+    JOIN salespersons sp ON sp.salesperson_id = (SELECT dbo.f_lead2salesperson(l.lead_id))
+    JOIN regions r ON sp.salesperson_region_id = r.region_id
+)
+SELECT * FROM LeadDetails;
+
+-- Calculate the average proposal cost by region
+WITH ProposalCosts AS (
+    SELECT p.proposal_total_cost, r.region_name
+    FROM proposals p
+    JOIN salespersons sp ON p.proposal_salesperson_id = sp.salesperson_id
+    JOIN regions r ON sp.salesperson_region_id = r.region_id
+)
+SELECT region_name, AVG(proposal_total_cost) AS avg_cost
+FROM ProposalCosts
+GROUP BY region_name;
 
