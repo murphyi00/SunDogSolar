0853571527e1f6ceb4bb766afcb38b17ded345e2
murphyi00 <71462707+murphyi00@users.noreply.github.com>
2024-09-13 16:12:57 -0700

    Update example_queries.sql

--- queries/example_queries.sql
+++ queries/example_queries.sql
@@ -1,1 +1,16 @@
+-- SunDogSolar: Example Queries
+
+-- Retrieve all customers
+SELECT * FROM customers;
+
+-- Retrieve all leads in New York
+SELECT * FROM leads WHERE lead_zipcode IN (SELECT zipcode_id FROM zipcodes WHERE zipcode_state_id = 'NY');
+
+-- Retrieve proposals with total cost greater than $15,000
+SELECT * FROM proposals WHERE proposal_total_cost > 15000;
+
+-- Retrieve salespersons and their regions
+SELECT sp.salesperson_firstname, sp.salesperson_lastname, r.region_name
+FROM salespersons sp
+JOIN regions r ON sp.salesperson_region_id = r.region_id;
 
